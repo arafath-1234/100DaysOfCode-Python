f8774aa690129c18f62c4ab47a22a57526401b33
MnArafat <60810145+arafath-1234@users.noreply.github.com>
2023-03-13 08:19:34 +0530

    Update and rename day1-BandNameGenerator to day1.py

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+print("What's the name of city you grew up in ?")
+city=input()
+print("What's your pet name ?")
+pet=input()
+print("Your band name could be "+city+" "+pet)
